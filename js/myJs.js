9a5f2a661bf4f33424846ed59bd9f874191d9da7
mindyp10101 <mindyp10101@gmail.com>
2023-02-12 18:33:38 -0500

    Update myJs.js

--- js/myJs.js
+++ js/myJs.js
@@ -182,13 +182,7 @@ $(document).ready(function () {
           background: '#fff url("img/iput-bg.jpg")',
           backdrop: `
                     rgba(0,0,123,0.4)
-                    url("img/inlove3.gif")
-                    right top
-                    no-repeat
-                  `,
-          backdrop: `
-                    rgba(0,0,123,0.4)
-                    url("img/inlove2.gif")
+                    url("img/cry.gif")
                     right top
                     no-repeat
                   `,
